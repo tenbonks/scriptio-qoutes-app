10c7cc8393b2887f8b6dbb8586e3a3bfd3e6697e
Ben Tonks <thebentonks@gmail.com>
2020-03-21 15:23:06 +0000

    Connected to mongoDB, rendered data to dom

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+# set config variables for mongodb connection
+app.config["MONGO_DBNAME"] = "scriptio"
+app.config["MONGO_URI"] = os.environ.get("MONGODB_URI")
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def hello():
-     return "Hello There!"
+@app.route("/get_qoutes")
+def get_qoutes():
+     return render_template("qoutes.html", posts=mongo.db.posts.find())
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
