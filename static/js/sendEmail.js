6fd5a81a1ac242495c9cf3981cdc8c925754b66b
Ben Tonks <thebentonks@gmail.com>
2020-04-26 01:57:11 +0100

    ran a beautifyer extension to indent code

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,20 +1,24 @@
-
 function sendMail(contactForm) {
     emailjs.send("gmail", "scriptio", {
-        "from_name": contactForm.from_name.value,
-        "from_email": contactForm.from_email.value,
-        "message": contactForm.message.value
-    }, "user_zryNwhPPBjpsX5Jah5OM5")
-    .then(
-        function(response) {
-            // aslong as the response is ok, the modal will close and reset the values
-            $("#modal-contact").modal("close")
-            document.getElementById("contactForm").reset();
-            M.toast({html: 'Sent!'})
-        },
-        function(error) {
-            M.toast({html: "Error!", error});
-        }
-    );
-    return false;  // To block from loading a new page
+            "from_name": contactForm.from_name.value,
+            "from_email": contactForm.from_email.value,
+            "message": contactForm.message.value
+        }, "user_zryNwhPPBjpsX5Jah5OM5")
+        .then(
+            function (response) {
+                // aslong as the response is ok, the modal will close and reset the values
+                $("#modal-contact").modal("close")
+                document.getElementById("contactForm").reset();
+                M.toast({
+                    html: 'Sent!'
+                })
+            },
+            function (error) {
+                M.toast({
+                    html: "Error!",
+                    error
+                });
+            }
+        );
+    return false; // To block from loading a new page
 }
